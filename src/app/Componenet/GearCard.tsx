edf2c830edc311a2c85b13bd7fd3daa5fd94bacb
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-12-14 00:26:44 +0500

    GearCard component

---
+++ src/app/Componenet/GearCard.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const GearCard = (props: any) => {
+    return (
+        <>
+            <div className="w-[310px]">
+                <Image
+                    src={props.Link}
+                    width={310}
+                    height={310}
+                    alt={props.alt}
+                />
+                <div className="flex text-[15px] justify-between mt-6">
+                    <div className="font-medium    ">
+                        <p className="">{props.d1}</p>
+                        <p className="text-[#757575] w-52">{props.d2}</p>
+                    </div>
+                    <p className="mr-3">{props.rate}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GearCard
