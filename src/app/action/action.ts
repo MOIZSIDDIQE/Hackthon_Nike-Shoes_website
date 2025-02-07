6be6675633728679bf3fe26690389ec01568eefa
MOIZSIDDIQE <moizpc2016@gmail.com>
2025-02-08 00:50:46 +0500

    Complete

---
+++ src/app/action/action.ts
@@ -0,0 +1,36 @@
+import { productType } from './../../../types/type';
+
+
+export const addToCart = (product :productType) => {
+    const cart  : productType[]= JSON.parse(localStorage.getItem('cart') || '[]') 
+
+    const existingProductIndex = cart.findIndex(item => item._id === product._id)
+
+    if(existingProductIndex > -1){
+        cart[existingProductIndex].inventory += 1
+    }else{
+        cart.push({...product, inventory: 1})
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+    export const removeFromCart = (productId : string) => {
+        let cart : productType[] = JSON.parse(localStorage.getItem('cart') || '[]')
+        cart = cart.filter(item => item._id !== productId)
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
+    
+    export const updateCartQuantity = (productId :string, quantity : number) => {
+        const cart : productType[] = JSON.parse(localStorage.getItem('cart') || '[]')
+        const productIndex = cart.findIndex(item => item._id === productId)
+    
+        if(productIndex > -1) {
+            cart[productIndex].inventory = quantity;
+            localStorage.setItem('cart', JSON.stringify(cart))
+        }
+    }
+    
+    export const getCartItems = () : productType[] => {
+        return JSON.parse(localStorage.getItem('cart') || '[]')
+}
