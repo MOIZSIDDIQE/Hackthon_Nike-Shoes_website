992e2e095f1a8b09b022ea0044b55ba37699db6b
MOIZSIDDIQE <moizpc2016@gmail.com>
2025-02-12 22:14:20 +0500

    Solve error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 
-import Image from "next/image";
-import Navbar from "./Navbar";
-import Hero from "./Home/Hero";
+
 import HomeBody from "./Home/page";
 import { fetchData } from "@/sanity/lib/fetch";
 import { products } from "@/sanity/lib/queries";
-import Example from "./example/Example";
 
 type productType = {
   _id : string,
